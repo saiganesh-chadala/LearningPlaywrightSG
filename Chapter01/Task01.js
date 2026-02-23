c385c15dc8eae1d753d9276ac77e3953917268ec
Sai Ganesh Chadala <saiganesh1598@gmail.com>
2026-02-23 13:56:18 +0530

    completed tasks 1&2

---
+++ Chapter01/Task01.js
@@ -0,0 +1,17 @@
+let score = 80;
+
+if (score >=90 && score<=100){
+    console.log('your grade is : A')
+}
+else if(score >=80 && score<=89){
+    console.log('your grade is : B')
+}
+else if(score >=70 && score<=79){
+    console.log('your grade is : C')
+}
+else if(score >=60 && score<=69){
+    console.log('your grade is : D')
+}
+else{
+    console.log('your grade is : F')
+}
