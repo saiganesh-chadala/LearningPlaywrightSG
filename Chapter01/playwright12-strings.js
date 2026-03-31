0ad25ad3a52a9ca94b9fcb64a44d68dced506bfb
Sai Ganesh Chadala <saiganesh1598@gmail.com>
2026-03-31 10:41:19 +0530

    March 19th Strings class-exercise

--- Chapter01/playwright12-strings.js
+++ Chapter01/playwright12-strings.js
@@ -26,3 +26,34 @@ console.log('----------------');
 
 //search accpets regex and returns index
 console.log(url.search(/login/));
+
+//execises of strings from taskboard
+
+let log = "[ERROR 2024-03-24 Test Case : Login Status: 500]";
+let status1 = log.match(/Status: (\d+)/)[1];
+console.log(status1);
+
+console.log('----------------');
+
+// let env = "staging";
+// let module = 'auth';
+// let count = 7;
+// let testid = `${env}_${module}_${String(count).padStart(3,"0")}`;
+// console.log(testid);
+
+console.log('----------------');
+let actual = "  PASS  ";
+let expected = "pass"
+let result = actual.trim().toLowerCase() === expected;
+console.log(result);
+
+console.log('----------------');
+let testURL = "https://skillspricer.com/search?query=login&page=2&sort=asc";
+let params = Object.fromEntries(testURL.split('?')[1].split('&').map(p => p.split('=')));
+console.log(params);
+
+console.log('----------------');
+
+let token = "Bearer vsdnmdcSW9.secret";
+let masked = token.replace(/(?<=Bearer).+/,"****REDACTED****")
+console.log(masked)
