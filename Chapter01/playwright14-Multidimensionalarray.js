ab64b1edeb9cc919816e97a23b3269a971556533
Sai Ganesh Chadala <saiganesh1598@gmail.com>
2026-04-01 09:52:17 +0530

    26th march Multi dimensional exercises

---
+++ Chapter01/playwright14-Multidimensionalarray.js
@@ -0,0 +1,33 @@
+console.log("--------------right triangle------------");
+let n = 5
+
+for(let i=1 ; i<=n ; i++){
+    let m = " ";
+    for(let j=1; j<=i ; j++){
+        m += "*";
+    }
+    console.log(m)
+}
+
+console.log("--------------left triangle------------");
+
+for( let i=n ; i>=1 ; i--){
+    let m =" ";
+    for(let j =i; j>=1 ; j--){
+        m += "*";
+    }
+    console.log(m);
+}
+
+console.log("--------------Pyramid------------");
+
+for(let i=1; i<=n; i++){
+    let row = "";
+    for(let j=1 ; j<=n-i ; j++){
+        row += " ";
+    }
+    for(let k=1; k<= 2*i-1; k++){
+        row += "*"
+    }
+    console.log(row)
+}
